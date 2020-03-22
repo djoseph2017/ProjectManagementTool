4c787fd055f890812856693a601e4fb862c20a5a
djoseph2017@my.fit.edu <djoseph2017@my.fit.edu>
2020-03-21 20:56:19 -0400

    adding a DB Object and MVC architecture

---
+++ src/main/java/com/springBoot/starter/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.springBoot.starter.repositories;
+
+import com.springBoot.starter.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+
+
+}
