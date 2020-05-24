d61f612ec7b9e22037db486102b4cf023a1774ab
djoseph2017@my.fit.edu <djoseph2017@my.fit.edu>
2020-05-24 16:23:17 -0400

    Adding the files to make store work and call REST API

---
+++ my-app/src/actions/projectActions.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import {GET_ERRORS} from "./typeActions";
+import errorReducer from "../reducers/errorReducer";
+
+export const createProject = (project, history) => async dispatch => {
+    try {
+        const res = await axios.post
+        ("http://localhost:8181/api/project",project)
+        history.push("/dashboard")
+    } catch (err) {
+        dispatch({
+            type:GET_ERRORS,
+            payload:err.response.data
+        })
+    }
+}
